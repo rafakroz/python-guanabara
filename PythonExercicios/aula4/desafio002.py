a930a847807b6c538e536371800c2fc369909422
Rafael Queiroz <76060193+rafakroz@users.noreply.github.com>
2024-08-02 14:32:18 -0300

    Add files via upload
    Arquivos iniciais

---
+++ PythonExercicios/aula4/desafio002.py
@@ -0,0 +1,6 @@
+nome = input('Qual o seu nome? ')
+
+# msg = 'Seja bem-vindo!'
+# print(msg, nome)
+
+print('Seja bem-vindo, {}!'.format(nome))
---
+++ index.py
@@ -0,0 +1,4 @@
+print("This line will be printed.")
+print(7+4)
+
+nome = input('Qual é o seu nome?')

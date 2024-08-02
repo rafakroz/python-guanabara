c743b920684b454e70192854293d12c000fcee17
Rafael Queiroz <76060193+rafakroz@users.noreply.github.com>
2024-08-02 16:29:14 -0300

    Add files via upload
    Pasta testes

---
+++ PythonTeste/aula06a.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+
+soma = n1 + n2
+
+#print(type(n1))
+
+# print('O resultado da soma é: ', soma)
+
+print('A soma entre {0} e {1} é: {2}.'.format(n1, n2, soma))

4b65b89463f865dd418e63e05ec46c91d8d453a9
Rafael Queiroz <rafakroz@gmail.com>
2024-08-04 14:08:26 -0300

    Desafios 012 e 013

--- PythonExercicios/aula07/desafio012.py
+++ PythonExercicios/aula07/desafio012.py
@@ -1,2 +1,8 @@
 # Faça um algoritmo que leia o preço de um produto e
 # mostre seu nome preço, com 5% de desconto
+
+prod = 'iPhone 15'
+valor = int(10000)
+valorDesconto = int(valor - (valor * 0.05))
+
+print(f'O valor do {prod} é R${valor} \nCom 5% de desconto fica R${valorDesconto}.')
--- PythonExercicios/aula07/desafio013.py
+++ PythonExercicios/aula07/desafio013.py
@@ -1,2 +1,9 @@
-# Faá um algoritmo que leia o salário de um funcionário e
+# Faça um algoritmo que leia o salário de um funcionário e
 # mostre seu novo salário , com 15% de aumento.
+
+nome = input('Informe o nome do funcionário: ')
+salario = int(input(f'Informe o salário base de {nome}: R$'))
+
+salarioReajustado = salario + (salario * 0.15)
+
+print(f'O salário de {nome} é de R${salario}. \nSerá reajustado em 15%, passando para R${salarioReajustado}.')

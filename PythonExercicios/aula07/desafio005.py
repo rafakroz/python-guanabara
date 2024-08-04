e1a70c2b06a3388c15f52c200cff5a70f072543f
Rafael Queiroz <rafakroz@gmail.com>
2024-08-04 12:54:22 -0300

    Desafios 005 e 006

--- PythonExercicios/aula07/desafio005.py
+++ PythonExercicios/aula07/desafio005.py
@@ -1,2 +1,10 @@
 # Faça um programa que leia um número inteiro e mostre ne tela
 # seu sucessor e seu antecessor
+
+num = int(input('Digite um número: '))
+
+anterior = num - 1
+proximo = num + 1
+
+print(f'O número anterior a {num} é: {anterior}')
+print(f'O número sucessor a {num} é: {proximo}')
--- PythonExercicios/aula07/desafio006.py
+++ PythonExercicios/aula07/desafio006.py
@@ -1,2 +1,12 @@
 # Crie um algoritmo que leia um número e mostre
 # o seu dobro, triplo e raiz quadrada.
+
+num = int(input('Digite um número: '))
+
+dobro = num * 2
+triplo = num * 3
+raiz = num ** 0.5
+
+print(f'O dobro de {num} é: {dobro}')
+print(f'O triplo de {num} é: {triplo}')
+print(f'A raiz quadrada de {num} é: {raiz}')

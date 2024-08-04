5febb11d786a2424244b5f90b890a86936c4eb1b
Rafael Queiroz <rafakroz@gmail.com>
2024-08-04 07:55:09 -0300

    Exercício 004 da lista separada

---
+++ PythonTeste/aula06b.py
@@ -0,0 +1,24 @@
+a = input('Digite algo: ')
+
+print('O tipo primitivo é ', type(a))
+
+# print('Só tem espaços?', a.isspace())
+print(f'Só tem espaços? {a.isspace()}')
+
+# print('É um número?', a.isnumeric())
+print(f'É um número? {a.isnumeric()}')
+
+# print('É alfabético?', a.isalpha())
+print(f'É alfabético? {a.isalpha()}')
+
+# print('É alfanumérico?', a.isalnum())
+print(f'É alfanumérico? {a.isalnum()}')
+
+# print('Está em caixa alta?', a.isupper())
+print(f'Está em caixa alta? {a.isupper()}')
+
+# print('Está em caixa baixa?', a.islower())
+print(f'Está em caixa baixa? {a.islower()}')
+
+# print('Se inicia com caixa alta?', a.istitle())
+print(f'Se inicia com caixa alta? {a.istitle()}')

9aeee76a0588f0a2dc83dde84b6a98e635280faa
Rafael Queiroz <rafakroz@gmail.com>
2024-08-04 13:17:24 -0300

    Desafios 007 e 008

--- PythonExercicios/aula07/desafio008.py
+++ PythonExercicios/aula07/desafio008.py
@@ -1,2 +1,10 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido
 # em centímetros e milímetros
+
+metro = int(input('Informe o(s) metro(s): '))
+
+cm = metro * 100
+mm = metro * 1000
+
+print(f'{metro}m em centímetros é: {cm}cm.')
+print(f'{metro}m em milímetros é: {mm}mm.')

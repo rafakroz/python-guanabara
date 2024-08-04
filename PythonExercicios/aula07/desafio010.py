3d89a24255bfad399e4061b9e0bed7c583c6f0b0
Rafael Queiroz <rafakroz@gmail.com>
2024-08-04 13:52:18 -0300

    Desafios 009, 010 e 011

--- PythonExercicios/aula07/desafio010.py
+++ PythonExercicios/aula07/desafio010.py
@@ -1,3 +1,10 @@
 # Crie um programa que leia quanto dinheiro uma tem na carteira e
 # mostre quantos dólares ela pode comprar
 # Considerar: USS 1 = R$ 3,27
+
+money = int(input('Informe o valor da carteira: '))
+
+dolar = 3.27
+cambio = money / dolar
+
+print(f'R${money}, no cotação de US${dolar}, equivale a US${cambio}.')
--- PythonExercicios/aula07/desafio011.py
+++ PythonExercicios/aula07/desafio011.py
@@ -1,3 +1,15 @@
 # Faça um programa que leia a LARGURA e a ALTURA de uma parede em metros,
 # calcule a sua área e a quantidade de tinta necessária para pintá-la.
 # Sabendo que cada litro de tinta, pinta uma área de 2m²
+
+largura = int(input('Largura da parede: '))
+altura = int(input('Altura da parede: '))
+
+area = largura * altura
+areaTinta = 2
+
+litros = area / areaTinta
+
+print(f'Para pintar {area}m² (Largura {largura}m / Altura {altura}m), é preciso {litros} litros de tinta.')
+
+

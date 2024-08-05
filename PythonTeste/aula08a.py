7b57f27b06157cea09e4b358c1d523f5a211ba17
Rafael Queiroz <rafakroz@gmail.com>
2024-08-04 21:52:16 -0300

    Aula 8

---
+++ PythonTeste/aula08a.py
@@ -0,0 +1,17 @@
+import math
+
+#Importanto método específico
+# from math import sqrt
+
+num = int(input('Digite um número: '))
+
+raiz = math.sqrt(num)
+
+print(f'A raiz quadrada de {num} é: {raiz:.2f}')
+
+#Arredondar para cima
+# print(f'A raiz quadrada de {num} é: {math.ceil(num)}')
+
+#Arrendondar para baixo
+# print(f'A raiz quadrada de {num} é: {math.floor(num)}')
+

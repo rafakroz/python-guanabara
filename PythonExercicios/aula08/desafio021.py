85457080553e5c0d2ed672629703fcbd6b9de199
Rafael Queiroz <rafakroz@gmail.com>
2024-08-05 22:43:23 -0300

    Inserido o caminho do mp3 do Mac˜

--- PythonExercicios/aula08/desafio021.py
+++ PythonExercicios/aula08/desafio021.py
@@ -6,7 +6,8 @@ import pygame
 pygame.init()
 
 # Carregar o arquivo
-pygame.mixer.music.load('/home/rafaelsilva/Projetos/python-guanabara/PythonExercicios/aula08/x.mp3')
+# pygame.mixer.music.load('/home/rafaelsilva/Projetos/python-guanabara/PythonExercicios/aula08/x.mp3')
+pygame.mixer.music.load('/Users/rafaelqueiroz/Documents/projects/python-guanabara/PythonExercicios/aula08/x.mp3')
 
 # Executar o arquivo
 pygame.mixer.music.play()

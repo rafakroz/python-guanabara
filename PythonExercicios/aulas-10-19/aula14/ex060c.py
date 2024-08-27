1d6d0ccc712e9ce6b8a4247d2dc754244163a5fb
Rafael Trabalho <rafakroz@gmail.com>
2024-08-27 16:08:03 -0300

    Aula 14. Exercício 060

---
+++ PythonExercicios/aulas-10-19/aula14/ex060c.py
@@ -0,0 +1,45 @@
+# Aula 14
+# Desafio 060
+# Exercício 14.4
+# Faça um programa que leia um número qualquer e mostre o seu fatorial.
+# Exemplo:
+# 5! = 5 x 4 x 3 x 2 x 1 = 120
+
+from time import sleep
+
+estilo = {'clear':        '\033[m',
+          'bold':      '\033[1m',
+          'blue':         '\033[34m',
+          'blueBold':     '\033[1;34m',
+          'cyan':         '\033[36m',
+          'cyanBold':     '\033[1;36m',
+          'green':        '\033[32m',
+          'greenBold':    '\033[1;32m',
+          'grey':        '\033[37m',
+          'greyBold':    '\033[1;37m',
+          'purple':     '\033[35m',
+          'purpleBold': '\033[1;35m',
+          'red':     '\033[31m',
+          'redBold': '\033[1;31m',
+          'yellow':      '\033[33m',
+          'yellowBold':  '\033[1;33m',
+          'blackWhite': '\033[7;30m'}
+
+asteriscos = 60
+
+num = int(input('Fatorial de: '))
+
+contador = num
+f = 1
+
+print(f'*' * asteriscos) # --------------------------------------
+
+while contador > 0:
+    print(f'{contador}', end='')
+    print(f' x ' if contador > 1 else ' = ', end='')
+    f *= contador
+    contador -= 1
+
+print(f'{f}')
+
+print(f'*' * asteriscos) # --------------------------------------
